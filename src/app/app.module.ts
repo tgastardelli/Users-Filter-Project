4d42aed246f4e58503178ded179c3619ec54598f
tgastardelli <tgastardelli@gmail.com>
2025-09-05 21:36:36 -0300

    feat: criando angular material module e components module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsModule } from './components/components.module';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ComponentsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
