8dcc638f3aabba5592dbc289d277ce729ebcf281
tgastardelli <tgastardelli@gmail.com>
2025-09-12 20:58:38 -0300

    feat: criando componente de filtro

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { AngularMaterialModule } from "../angular-material/angular-material.module";
+import { FilterComponent } from './filter/filter.component';
+import { CommonModule } from "@angular/common";
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
-    UserDetailsComponent
+    UserDetailsComponent,
+    FilterComponent,
+    UsersListComponent
   ],
-  imports: [AngularMaterialModule],
-  exports: [UserDetailsComponent]
+  imports: [AngularMaterialModule, CommonModule],
+  exports: [UserDetailsComponent, FilterComponent]
 })
 export class ComponentsModule {}
---
+++ src/app/components/filter/filter.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { IFood } from 'src/app/interfaces/food/food.interface';
+
+@Component({
+  selector: 'app-filter',
+  templateUrl: './filter.component.html',
+  styleUrls: ['./filter.component.scss'],
+})
+export class FilterComponent {
+  public foods: IFood[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' },
+  ];
+}
