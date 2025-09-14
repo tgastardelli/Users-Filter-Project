ad96bee3dd962fdecc8e7e213e480ec40e48db12
tgastardelli <tgastardelli@gmail.com>
2025-09-13 21:26:11 -0300

    feat: criação do filtro por status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,12 +37,25 @@ export class AppComponent implements OnInit {
     let filteredList: IUser[] = [];
 
     filteredList = this.filterUsersListByName(filterOptions.name, usersList);
+    filteredList = this.filterUsersListByStatus(filterOptions.status, filteredList);
+
+    return filteredList;
+  }
+
+  filterUsersListByStatus(status: boolean | undefined, usersList: IUser[]): IUser[] {
+    const STATUS_NOT_SELECTED = status === undefined;
+
+    if (STATUS_NOT_SELECTED) {
+      return usersList;
+    }
+
+    const filteredList = usersList.filter((user) => user.ativo === status);
 
     return filteredList;
   }
 
   public filterUsersListByName(name: string | undefined, usersList: IUser[]): IUser[] {
-    const NAME_NOT_TYPED = name === undefined
+    const NAME_NOT_TYPED = name === undefined;
 
     if (NAME_NOT_TYPED) {
       return usersList;
@@ -51,7 +64,6 @@ export class AppComponent implements OnInit {
     const filteredList = usersList.filter((user) => user.nome.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
 
     return filteredList;
-
   }
 
 
